b9866c4236cbcef10dc49bcbe263403f89be6fd9
nikkhil31 <nikhilimbad@gmail.com>
2022-07-23 01:18:20 +0530

    frontend with redux toolkit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { emailRegex } from "../utils/utils";
 
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="container">
+      <div className="loginBox">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-field">
+            <input
+              type={"text"}
+              placeholder="Email"
+              {...register("email", { required: true, pattern: emailRegex })}
+            />
+            <span>
+              {errors.email?.type === "required" && "Email is Required!"}
+              {errors.email?.type === "pattern" && "Please enter valid email!"}
+            </span>
+          </div>
+          <div className="form-field">
+            <input
+              type={"password"}
+              placeholder="Password"
+              {...register("password", { required: true })}
+            />
+            <span>
+              {errors.password?.type === "required" && "Password is Required!"}
+            </span>
+          </div>
+          <div className="form-field">
+            <input type={"submit"} name="submit" value={"Submit"} />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
